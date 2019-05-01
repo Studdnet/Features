c67fd81d98dc2624dd8a3cff2ee3d2295cf4e6b5
Studdnet <st.codding@gmail.com>
2019-05-01 12:41:32 +0300

    docs: fix script.js

--- 3D_card_for_portfolio/js/script.js
+++ 3D_card_for_portfolio/js/script.js
@@ -1,4 +1,4 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />
 $('.card').on('click', function() {
-    $('.container').toggleClass('.container-origin');
+    $('.container').toggleClass('container-origin');
 })
